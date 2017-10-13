3952d96056fa062402e53adc82087784109004f1
londeroner <31966757+londeroner@users.noreply.github.com>
2017-10-13 14:33:53 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,16 +11,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[,] MainMas = new int[10, 10];
+            Console.Write("Введите размерности для случайно сгенерированной матрицы через пробел: ");
+            string[] str = Console.ReadLine().Split();
+            int L1 = int.Parse(str[0]);
+            int L2 = int.Parse(str[1]);
+            int[,] MainMas = new int[L1, L2];
             Random rnd = new Random();
-            int k=0, m=0, min = 101;
+            int k = 0, m = 0, min = 101;
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < L1; i++)
             {
-                for (int j = 0; j < 10; j++)
+                for (int j = 0; j < L2; j++)
                 {
                     MainMas[i, j] = rnd.Next(1, 100);
-                    if (MainMas[i,j] < min)
+                    if (MainMas[i, j] < min)
                     {
                         min = MainMas[i, j];
                         k = i;
@@ -28,10 +33,10 @@ namespace ConsoleApp1
                 }
             }
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < L1; i++)
             {
                 Console.WriteLine();
-                for (int j = 0; j < 10; j++)
+                for (int j = 0; j < L2; j++)
                 {
                     Console.Write("{0} ", MainMas[i, j]);
                 }
@@ -40,17 +45,17 @@ namespace ConsoleApp1
             Console.WriteLine();
             Console.WriteLine();
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < L2; i++)
             {
                 int t = MainMas[0, i];
-                MainMas[0, i] = MainMas[9,9-i];
-                MainMas[9, 9 - i] = t;
+                MainMas[0, i] = MainMas[L1 - 1, L2 - i - 1];
+                MainMas[L1 - 1, L2 - i - 1] = t;
             }
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < L1; i++)
             {
                 Console.WriteLine();
-                for (int j = 0; j < 10; j++)
+                for (int j = 0; j < L2; j++)
                 {
                     Console.Write("{0} ", MainMas[i, j]);
                 }
@@ -59,23 +64,23 @@ namespace ConsoleApp1
             Console.WriteLine();
             Console.WriteLine();
 
-            int[,] SecMas = new int[9, 9];
+            int[,] SecMas = new int[L1 - 1, L2 - 1];
 
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < L1; i++)
             {
-                for (int j = 0; j < 9; j++)
+                for (int j = 0; j < L2; j++)
                 {
                     if (i >= k && j >= m)
                     {
-                        SecMas[i, j] = MainMas[i + 1, j + 1];                   
+                        SecMas[i - 1, j - 1] = MainMas[i, j];
                     }
                     else if (i >= k && j < m)
                     {
-                        SecMas[i, j] = MainMas[i+1, j];
+                        SecMas[i - 1, j] = MainMas[i, j];
                     }
                     else if (i < k && j >= m)
                     {
-                        SecMas[i, j] = MainMas[i, j + 1];
+                        SecMas[i, j - 1] = MainMas[i, j];
                     }
                     else
                     {
@@ -84,10 +89,10 @@ namespace ConsoleApp1
                 }
             }
 
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < L1; i++)
             {
                 Console.WriteLine();
-                for (int j = 0; j < 9; j++)
+                for (int j = 0; j < L2; j++)
                 {
                     Console.Write("{0} ", SecMas[i, j]);
                 }
@@ -95,56 +100,7 @@ namespace ConsoleApp1
 
             Console.WriteLine();
             Console.WriteLine();
-            Console.WriteLine("Введите массив 9 на 9");
-            Console.WriteLine();
-
-            int[,] InMas = new int[9,9];
-            int CountStroke = 0;
-
-            while (CountStroke < 9)
-            {
-                string[] str = Console.ReadLine().Split();
-
-                for (int i = 0; i < 9; i++)
-                {
-                    if (i != 8 && CountStroke != 8) InMas[CountStroke, i] = int.Parse(str[i]);
-                    else if (i == 8 && CountStroke != 8)
-                    {
-                        string[] strdop = str[str.Length - 1].Split(',');
-                        InMas[CountStroke, i] = int.Parse(strdop[0]);
-                    }
-                    else {
-                        string[] strdop = str[str.Length - 1].Split('.');
-                        InMas[CountStroke, i] = int.Parse(strdop[0]);
-                    }
-                    
-                }
-
-                CountStroke++;
-            }
-
-            int[,] ResMas = new int[9, 9];
-
-            for (int i = 0; i < 9; i++)
-            {
-                int res = 0;
-                for (int j = 0; j < 9; j++)
-                {
-                    for (int l = 0; l < 9; l++)
-                    {
-                        res += SecMas[i, j] * InMas[j, l];
-                    }
-                }
-            }
-
-            for (int i = 0; i < 9; i++)
-            {
-                Console.WriteLine();
-                for (int j = 0; j < 9; j++)
-                {
-                    Console.Write("{0} ", ResMas[i, j]);
-                }
-            }
+            
 
             Console.ReadLine();
         }
