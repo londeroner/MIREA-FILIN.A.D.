206f7afd2d37c8defc96859e024c1918023e0b30
londeroner <31966757+londeroner@users.noreply.github.com>
2017-11-24 16:53:17 +0300

    Update lsn.c

--- lsn.c
+++ lsn.c
@@ -11,6 +11,6 @@ int main ()
     struct stat *pDire;
     dirname = opendir("/");
     pDire = readdir(dirname);
-    while ((pDire = readdir(dirname)) != 0) 
-          printf("%s %s %s %s %s %s %s %s %s %s", pDire->st_mode, pDire->st_ino, pDire->st_dev, pDire->st_nlink, pDire->st_uid, pDire->st_gid, pDire->st_size, pDire->st_ctime);
+    while ((pDire = readdir(dirname)) != NULL) 
+          printf("%hu %d %d %d %d %d %d %d %d %d", pDire->st_mode, pDire->st_ino, pDire->st_dev, pDire->st_nlink, pDire->st_uid, pDire->st_gid, pDire->st_size, pDire->st_ctime);
 }
