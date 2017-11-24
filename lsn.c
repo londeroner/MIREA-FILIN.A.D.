c8894b367f6738e44764780be3f29d4cc1e5740b
londeroner <31966757+londeroner@users.noreply.github.com>
2017-11-24 17:01:30 +0300

    Update lsn.c

--- lsn.c
+++ lsn.c
@@ -12,5 +12,5 @@ int main ()
     dirname = opendir("/");
     pDire = readdir(dirname);
     while ((pDire = readdir(dirname)) != NULL) 
-          printf("%hu %d %d %d %d %d %d %d %d %d", pDire->st_mode, pDire->st_ino, pDire->st_dev, pDire->st_nlink, pDire->st_uid, pDire->st_gid, pDire->st_size, pDire->st_ctime);
+          printf("%hu %ld %d %d %d %d %d %d %d %s", pDire->st_mode, pDire->st_ino, pDire->st_dev, pDire->st_nlink, pDire->st_uid, pDire->st_gid, pDire->st_size, ctime(pDire->st_ctime));
 }
